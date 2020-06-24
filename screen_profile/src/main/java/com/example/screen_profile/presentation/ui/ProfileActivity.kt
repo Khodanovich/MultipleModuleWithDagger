990a45c644d7978acbaf409131e16fcfb8c20f27
khodanovich <a.khodanovich@tinkoff.ru>
2020-06-24 15:54:49 +0500

    fix

--- screen_profile/src/main/java/com/example/screen_profile/presentation/ui/ProfileActivity.kt
+++ screen_profile/src/main/java/com/example/screen_profile/presentation/ui/ProfileActivity.kt
@@ -1,7 +1,5 @@
 package com.example.screen_profile.presentation.ui
 
-import android.content.Context
-import android.content.Intent
 import com.example.core_ui.presentation.di.UIComponent
 import com.example.core_ui.presentation.extentions.injectViewModel
 import com.example.core_ui.presentation.ui.base.BaseActivity
@@ -13,12 +11,6 @@ import com.example.screen_profile.presentation.vm.ProfileViewModel
  * @author a.khodanovich
  */
 class ProfileActivity : BaseActivity<ProfileViewModel>() {
-
-    companion object {
-
-        fun newIntent(context: Context) = Intent(context, ProfileActivity::class.java)
-
-    }
 
     override val viewModel: ProfileViewModel by injectViewModel()
     override var diComponent: UIComponent = ProfileScreenComponentHolder.get()
